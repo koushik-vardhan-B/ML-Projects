8cd27c0b045afefcfc75de6680bb2b3fe5384c81
koushik-vardhan-B <bandikoushik781@gmail.com>
2025-06-07 19:49:47 +0530

    Hyperparameter tuning

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import pickle
 from src.exception import CustomException
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
     """
@@ -25,7 +26,7 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e, sys) from e
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     """
     Evaluate multiple regression models and return their performance metrics.
     
@@ -40,10 +41,18 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
         report = {}
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            model.fit(X_train, y_train)
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+            
+            # model.fit(X_train, y_train)
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
-            trian_model_score = r2_score(y_train, y_train_pred)
+            train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
             report[list(models.keys())[i]] = test_model_score
         return report
