3722875149d73dd92fbaa884e1269e9ba0b585c5
koushik-vardhan-B <bandikoushik781@gmail.com>
2025-06-07 15:29:12 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,8 +7,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
-
-
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+from src.utils import evaluate_models
 from dataclasses import dataclass
 @dataclass
 class DataIngestionConfig:
@@ -45,10 +46,17 @@ class DataIngestion:
         
 if __name__ == "__main__":
     obj = DataIngestion()
-    train_data,test_data=obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
     
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, preprocessor_path = data_transformation.initiate_data_transformation(train_data, test_data)
+    
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(
+        train_array=train_arr, 
+        test_array=test_arr, 
+        preprocessor_path=preprocessor_path
+    ))
 # This code is part of a data ingestion module that reads a dataset, splits it into training and testing sets, and saves them to specified paths.
 # It uses pandas for data manipulation and sklearn for splitting the dataset.
 # The code also includes error handling and logging for better traceability.
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 import pickle
 from src.exception import CustomException
 import dill
+from sklearn.metrics import r2_score
 
 def save_object(file_path,obj):
     """
@@ -23,3 +24,28 @@ def save_object(file_path,obj):
             dill.dump(obj, file_obj)
     except Exception as e:
         raise CustomException(e, sys) from e
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    """
+    Evaluate multiple regression models and return their performance metrics.
+    
+    Parameters:
+    X_train: Training feature set.
+    y_train: Training target variable.
+    X_test: Testing feature set.
+    y_test: Testing target variable.
+    models: Dictionary of model names and their corresponding model instances.
+    """
+    try:
+        report = {}
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            model.fit(X_train, y_train)
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+            trian_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+            report[list(models.keys())[i]] = test_model_score
+        return report
+    except Exception as e:
+        raise CustomException(e, sys) 
